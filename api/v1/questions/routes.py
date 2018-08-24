bde1abe694aba6f82f09cf357c941755d547fa11
root <root@localhost.localdomain>
2018-08-24 23:20:41 +0300

    Get a question #160023188

--- api/v1/questions/routes.py
+++ api/v1/questions/routes.py
@@ -1,6 +1,8 @@
 from flask import Flask,jsonify
+from flask_restful import Resource, Api,abort
 
 app = Flask(__name__)
+api = Api(app)
 
 #questions held in a dictionary
 posts = [
@@ -26,10 +28,17 @@ posts = [
 	}
 ]
 
+#Get all questions
 @app.route('/api/v1/questions', methods = ['GET'])
-def questions():
+def get_questions():
 	#return all questions
 	return jsonify({'questions':posts})
+
+#Get a question
+@app.route('/api/v1/questions/<int:question_id>', methods = ['GET'])
+def get_question(question_id):
+	#return a question
+	return jsonify({'questions':posts[question_id]})
 
 if __name__ == "__main__":
 	app.run(debug=True)
