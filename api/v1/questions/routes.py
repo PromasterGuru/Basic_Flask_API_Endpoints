ec34f4143f3900fbbf3a23f72c804d6ca2549fd1
root <root@localhost.localdomain>
2018-08-25 11:27:54 +0300

    Added Api endpoint for posting a question #160025971

--- api/v1/questions/routes.py
+++ api/v1/questions/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api,abort
 
 app = Flask(__name__)
@@ -32,13 +32,30 @@ posts = [
 @app.route('/api/v1/questions', methods = ['GET'])
 def get_questions():
 	#return all questions
-	return jsonify({'questions':posts})
+	return jsonify({'questions':posts}),200 #Success/Ok
 
 #Get a question
 @app.route('/api/v1/questions/<int:question_id>', methods = ['GET'])
 def get_question(question_id):
+	post = [post for post in posts if post['question_id'] == question_id]
+	if len(post) == 0:
+		abort(404) #Not found
 	#return a question
-	return jsonify({'questions':posts[question_id]})
+	return jsonify({'questions':post[0]}),200 #Success/Ok
+
+#Post a question
+@app.route('/api/v1/questions', methods = ['POST'])
+def post_question():
+	if not request.json or not 'user_id' in request.json:
+		abort(400) #Bad request
+	question = {
+		'question_id':posts[-1]['question_id']+1,
+		'user_id':request.json['user_id'],
+		'questions':request.json.get('questions',"")
+	}
+	posts.append(question)
+	#return all questions
+	return jsonify({'questions':posts}),201 #created
 
 if __name__ == "__main__":
 	app.run(debug=True)
