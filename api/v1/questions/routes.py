36b75b4b8a75147d21a891e62ba9c10e5cc8d40c
root <root@localhost.localdomain>
2018-08-25 17:14:21 +0300

    Api endpoint for updating answers #160037850

--- api/v1/questions/routes.py
+++ api/v1/questions/routes.py
@@ -3,33 +3,63 @@ from flask_restful import Resource, Api,abort
 
 app = Flask(__name__)
 api = Api(app)
-
-#Questions held in a dictionary
+#Users
+users = [
+	{
+	  'user_id': 1001,
+	  'username':'promaster',
+	  'password': 'promaster2018'
+	},
+	{
+	  'user_id': 1002,
+	  'username':'paul',
+	  'password': 'paul2018'
+	},
+	{
+	  'user_id': 1003,
+	  'username':'James',
+	  'password': 'james2018'
+	},
+	{
+	  'user_id': 1004,
+	  'username':'daniel',
+	  'password': 'daniel2018'
+	}
+]
+#Questions
 posts = [
 	{
 	  'question_id':101,
-	  'user_id':300,
+	  'user_id':users[0]['user_id'],
 	  'questions':'What is REST?'
 	},
 	{
 	  'question_id':102,
-	  'user_id':301,
+	  'user_id':users[1]['user_id'],
 	  'questions':'What is Api?'
 	},
 	{
 	  'question_id':103,
-	  'user_id':302,
+	  'user_id':users[2]['user_id'],
 	  'questions':'What RESTful Api?'
 	},
 	{
 	  'question_id':104,
-	  'user_id':303,
+	  'user_id':users[2]['user_id'],
 	  'questions':'What are the commonly used webservices?'
 	}
 ]
 
-#Qnswers held in a dictionary
-answers = []
+#Qnswers
+answers = [
+	{
+	  'question_id': posts[0]['question_id'],
+	  'user_id': users[0]['user_id'],
+	  'answer_id': 200,
+	  'answer':"REpresentational State Transfer",
+	  'complete':False
+	}
+]
 
 #Get all questions
 @app.route('/questions', methods = ['GET'])
@@ -71,7 +101,10 @@ def post_answer(questionId):
 	else:
 		answer = {
 			'question_id':questionId,
-			'answer':request.json.get('answers',"")
+			'user_id':request.json.get("userId"),
+			'answer_id':request.json.get("answerId"),
+			'answer':request.json.get('answers',""),
+			'complete':False
 			}
 	answers.append(answer)
 	return jsonify({"Answers":answers}),200 #OK
@@ -84,7 +117,18 @@ def delete_question(questionId):
 		abort(404) #Not found
 	posts.remove(post[0])
 	return jsonify({'Questions': posts}),200 #OK
-	return
-	
+
+#Mark answer as complet or update the answer
+@app.route('/questions/<int:questionId>/answers/<int:answerId>',methods = ['PUT'])
+def update_question(questionId,answerId):
+	if not request.json or not 'answers' in request.json:
+		abort(400) #Bad request
+	answer = [answer for answer in answers if (answer['question_id'] == questionId and answer['answer_id'] == answerId)]
+	if len(answer) == 0:
+		abort(404) #Not found
+	pos = answers.index(answer[0])
+	answers[pos]['answer'] = request.json.get('answers')
+	return jsonify({"Answers": answers}),200 #OK
+
 if __name__ == "__main__":
 	app.run(debug=True)
